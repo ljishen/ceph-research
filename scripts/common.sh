98059d5e9ea5513e8cdf0350092d4dc4f787e6e1
Jianshen Liu <jliu120@ucsc.edu>
2020-12-31 00:42:06 -0700

    Create parallel-exec-script for executing local scripts on remote hosts

--- scripts/common.sh
+++ scripts/common.sh
@@ -4,7 +4,12 @@
 
 set -euo pipefail
 
-common::info() { printf "\\033[1;32m[INFO] %s\\033[0m\\n" "$*"; }
+if (( ${INFO_LEVEL:-0} )); then
+  _INFO_PREFIX="$(printf '%0.s>' $(seq 1 "$INFO_LEVEL")) "
+fi
+common::info() {
+  printf "\\033[1;32m[INFO] %s%s\\033[0m\\n" "${_INFO_PREFIX:-}" "$*"
+}
 common::debug() { printf "\\033[1;30m[DEBUG] %s\\033[0m\\n" "$*"; }
 common::err() {
   local -ir exit_status="$1"
@@ -38,6 +43,44 @@ common::print_array() {
   echo "[${joined%, }]"
 }
 
+readonly ERR_CMDLINE_PARAM_UNKNOW=2
+readonly ERR_CMDLINE_PARAM_MISSING=3
+common::parse_cmdline_params() {
+  local -n _param_arr=$1
+  shift
+
+  # loop positional params: https://unix.stackexchange.com/a/314041
+  local -i return_status=0
+  for (( idx = 1; idx <= $#; idx++ )); do
+    if [[ "${!idx}" == @(--hosts|--host) ]]; then
+      _param_arr+=("${!idx}")
+      (( idx++ ))
+      if [[ -z "${!idx:-}" ]]; then
+        common::err $ERR_CMDLINE_PARAM_MISSING \
+          "Missing parameter for '${_param_arr[-1]}'"
+      fi
+      _param_arr+=("${!idx}")
+      continue
+    fi
+
+    if [[ "${!idx}" =~ ^(--hosts=|--host=) ]]; then
+      _param_arr+=("${!idx}")
+      continue
+    fi
+
+    # check if a function is defined
+    #   https://stackoverflow.com/a/85932
+    if ! declare -f handle_add_cmdline_param >/dev/null; then
+      common::err $ERR_CMDLINE_PARAM_UNKNOW "Unsupport parameter '${!idx}'"
+    fi
+
+    handle_add_cmdline_param "${!idx}" || return_status=$?
+    if (( return_status )); then
+      common::err $ERR_CMDLINE_PARAM_UNKNOW "Unsupport parameter '${!idx}'"
+    fi
+  done
+}
+
 
 export SSH_COMM_OPTIONS=(
   -o "GlobalKnownHostsFile=/dev/null"
@@ -46,4 +89,4 @@ export SSH_COMM_OPTIONS=(
   -o "StrictHostKeyChecking=no"
   -o "UserKnownHostsFile=/dev/null"
 )
-export ERR_STATUS_START=2
+export ERR_STATUS_START=4
