9ac69abea5a7cb97ced84c35c366e819e3a3a16c
Jianshen Liu <jliu120@ucsc.edu>
2021-10-23 22:04:44 -0400

    Backup baseline throughputs for nvmeof for ramdisk and hdd target

---
+++ benchmark_data/data/osd-on-bluefield_2/cloudlab/server:r7525-client:r7525/tp_baseline/nvmeof_rdma_roce/ramdisk_target/fio_command.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+readonly FIO_BLOCK_SIZES=("4k" "16k" "64k" "256k" "1m" "4m" "16m")
+readonly TASKSET_CPU_LIST=32-63,96-127
+readonly FIO_COMMAND="\
+   sudo taskset --cpu-list $TASKSET_CPU_LIST \
+    fio \
+    --iodepth=64 \
+    --ioengine=libaio \
+    --direct=1 \
+    --invalidate=1 \
+    --fsync_on_close=1 \
+    --randrepeat=1 \
+    --norandommap \
+    --group_reporting \
+    --name nvmeof_ramdisk \
+    --filename=/dev/nvme0n1 \
+    --time_based \
+    --runtime=30 \
+    --output-format=json+"
+
+
+for num_jobs in $(seq 1 9); do
+  cd ../numjobs="$num_jobs"
+
+  for bs in "${FIO_BLOCK_SIZES[@]}"; do
+    for _ in $(seq 1 2); do
+      $FIO_COMMAND  \
+        --rw=randread \
+        --bs="$bs" \
+        --numjobs="$num_jobs" \
+        --output=fio_randread_"$bs".log
+      sleep 10
+    done
+  done
+  
+  for bs in "${FIO_BLOCK_SIZES[@]}"; do
+    for _ in $(seq 1 2); do
+      $FIO_COMMAND \
+        --rw=randwrite \
+        --bs="$bs" \
+        --numjobs="$num_jobs" \
+        --output=fio_randwrite_"$bs".log
+      sleep 10
+    done
+  done
+done
