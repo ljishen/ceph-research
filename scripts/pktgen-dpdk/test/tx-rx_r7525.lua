82d9a9974e4cb6870c1bc7dfbd5cb8464e110bf3
Jianshen Liu <jliu120@ucsc.edu>
2021-11-22 01:26:42 -0500

    Update the pktgen test scripts

--- scripts/pktgen-dpdk/test/tx-rx_r7525.lua
+++ scripts/pktgen-dpdk/test/tx-rx_r7525.lua
@@ -3,127 +3,32 @@ package.path = package.path ..";?.lua;test/?.lua;app/?.lua;"
 -- manual for more comment options.
 require "Pktgen"
 
-local eth_dst_addr = "0c:42:a1:a4:89:8c"
-local eth_src_addr = "0c:42:a1:a4:8a:58"
-local pkt_size = 4096
+local eth_dst_addr = "0c:42:a1:a4:89:a4"
+local eth_src_addr = "0c:42:a1:a4:89:a8"
+local seq_cnt = 16
+local pkt_size = 8192
 
-pktgen.set("all", "seq_cnt", 8);
+
+pktgen.set("0", "seq_cnt", seq_cnt)
 
 local seq_table = {}
-seq_table[0] = {
-  ["eth_dst_addr"] = eth_dst_addr,
-  ["eth_src_addr"] = eth_src_addr,
-  ["ip_dst_addr"] = "10.10.2.1",
-  ["ip_src_addr"] = "10.10.2.2",
-  ["sport"] = 1234,
-  ["dport"] = 5678,
-  ["ethType"] = "ipv4",
-  ["ipProto"] = "udp",
-  ["vlanid"] = 1,
-  ["pktSize"] = pkt_size,
-  ["gtpu_teid"] = 0
-}
-seq_table[1] = {
-  ["eth_dst_addr"] = eth_dst_addr,
-  ["eth_src_addr"] = eth_src_addr,
-  ["ip_dst_addr"] = "10.10.2.1",
-  ["ip_src_addr"] = "10.10.2.2",
-  ["sport"] = 1234,
-  ["dport"] = 5678,
-  ["ethType"] = "ipv4",
-  ["ipProto"] = "udp",
-  ["vlanid"] = 1,
-  ["pktSize"] = pkt_size,
-  ["gtpu_teid"] = 0
-}
-seq_table[2] = {
-  ["eth_dst_addr"] = eth_dst_addr,
-  ["eth_src_addr"] = eth_src_addr,
-  ["ip_dst_addr"] = "10.10.2.1",
-  ["ip_src_addr"] = "10.10.2.2",
-  ["sport"] = 1234,
-  ["dport"] = 5678,
-  ["ethType"] = "ipv4",
-  ["ipProto"] = "udp",
-  ["vlanid"] = 1,
-  ["pktSize"] = pkt_size,
-  ["gtpu_teid"] = 0
-}
-seq_table[3] = {
-  ["eth_dst_addr"] = eth_dst_addr,
-  ["eth_src_addr"] = eth_src_addr,
-  ["ip_dst_addr"] = "10.10.2.1",
-  ["ip_src_addr"] = "10.10.2.2",
-  ["sport"] = 1234,
-  ["dport"] = 5678,
-  ["ethType"] = "ipv4",
-  ["ipProto"] = "udp",
-  ["vlanid"] = 1,
-  ["pktSize"] = pkt_size,
-  ["gtpu_teid"] = 0
-}
-seq_table[4] = {
-  ["eth_dst_addr"] = eth_dst_addr,
-  ["eth_src_addr"] = eth_src_addr,
-  ["ip_dst_addr"] = "10.10.2.1",
-  ["ip_src_addr"] = "10.10.2.2",
-  ["sport"] = 1234,
-  ["dport"] = 5678,
-  ["ethType"] = "ipv4",
-  ["ipProto"] = "udp",
-  ["vlanid"] = 1,
-  ["pktSize"] = pkt_size,
-  ["gtpu_teid"] = 0
-}
-seq_table[5] = {
-  ["eth_dst_addr"] = eth_dst_addr,
-  ["eth_src_addr"] = eth_src_addr,
-  ["ip_dst_addr"] = "10.10.2.1",
-  ["ip_src_addr"] = "10.10.2.2",
-  ["sport"] = 1234,
-  ["dport"] = 5678,
-  ["ethType"] = "ipv4",
-  ["ipProto"] = "udp",
-  ["vlanid"] = 1,
-  ["pktSize"] = pkt_size,
-  ["gtpu_teid"] = 0
-}
-seq_table[6] = {
-  ["eth_dst_addr"] = eth_dst_addr,
-  ["eth_src_addr"] = eth_src_addr,
-  ["ip_dst_addr"] = "10.10.2.1",
-  ["ip_src_addr"] = "10.10.2.2",
-  ["sport"] = 1234,
-  ["dport"] = 5678,
-  ["ethType"] = "ipv4",
-  ["ipProto"] = "udp",
-  ["vlanid"] = 1,
-  ["pktSize"] = pkt_size,
-  ["gtpu_teid"] = 0
-}
-seq_table[7] = {
-  ["eth_dst_addr"] = eth_dst_addr,
-  ["eth_src_addr"] = eth_src_addr,
-  ["ip_dst_addr"] = "10.10.2.1",
-  ["ip_src_addr"] = "10.10.2.2",
-  ["sport"] = 1234,
-  ["dport"] = 5678,
-  ["ethType"] = "ipv4",
-  ["ipProto"] = "udp",
-  ["vlanid"] = 1,
-  ["pktSize"] = pkt_size,
-  ["gtpu_teid"] = 0
-}
-
-pktgen.seqTable(0, "all", seq_table[0]);
-pktgen.seqTable(1, "all", seq_table[1]);
-pktgen.seqTable(2, "all", seq_table[2]);
-pktgen.seqTable(3, "all", seq_table[3]);
-pktgen.seqTable(4, "all", seq_table[4]);
-pktgen.seqTable(5, "all", seq_table[5]);
-pktgen.seqTable(6, "all", seq_table[6]);
-pktgen.seqTable(7, "all", seq_table[7]);
+for i=0,seq_cnt - 1 do
+	seq_table[i] = {
+	  ["eth_dst_addr"] = eth_dst_addr,
+	  ["eth_src_addr"] = eth_src_addr,
+	  ["ip_dst_addr"] = "10.10.1.2",
+	  ["ip_src_addr"] = "10.10.1.1",
+	  ["sport"] = 12340 + i,
+	  ["dport"] = 56780 + i,
+	  ["ethType"] = "ipv4",
+	  ["ipProto"] = "udp",
+	  ["vlanid"] = i,
+	  ["pktSize"] = pkt_size,
+	  ["gtpu_teid"] = i
+	}
+	pktgen.seqTable(i, "0", seq_table[i])
+end
 
 -- 32 is the largest burst value
 --   https://github.com/pktgen/Pktgen-DPDK/blob/pktgen-21.05.0/app/pktgen-constants.h#L23
-pktgen.set("all", "burst", 32);
+pktgen.set("0", "burst", 32)
