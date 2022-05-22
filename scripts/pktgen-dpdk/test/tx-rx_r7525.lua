d26cb12da1bf3be2cd3b3d7eaea5c59d07020fe0
Jianshen Liu <jliu120@ucsc.edu>
2022-05-21 21:35:28 -0400

    Update the pktgen configuration scripts

--- scripts/pktgen-dpdk/test/tx-rx_r7525.lua
+++ scripts/pktgen-dpdk/test/tx-rx_r7525.lua
@@ -3,27 +3,28 @@ package.path = package.path ..";?.lua;test/?.lua;app/?.lua;"
 -- manual for more comment options.
 require "Pktgen"
 
-local eth_dst_addr = "0c:42:a1:a4:89:cc"
-local eth_src_addr = "0c:42:a1:a4:8a:04"
-local pkt_size = 16
+local eth_dst_addr = "0c:42:a1:a4:89:e0"
+local eth_src_addr = "0c:42:a1:a4:89:c8"
+local pkt_size = 64
 
 -- 16 is the max number of sequences
 -- https://github.com/pktgen/Pktgen-DPDK/blob/dev/app/pktgen-cmds.c#L2881
 local seq_cnt = 16
 pktgen.set("0", "seq_cnt", seq_cnt)
+pktgen.vlan("0", "on");
 
 local seq_table = {}
 for i=0,seq_cnt - 1 do
   seq_table[i] = {
     ["eth_dst_addr"] = eth_dst_addr,
     ["eth_src_addr"] = eth_src_addr,
-    ["ip_dst_addr"] = "10.10.1.111",
+    ["ip_dst_addr"] = "10.10.1." .. (111 + i),
     ["ip_src_addr"] = "10.10.1.222",
-    ["dport"] = 12340,
-    ["sport"] = 56780,
+    ["dport"] = 12340 + i,
+    ["sport"] = 56780 + i,
     ["ethType"] = "ipv4",
     ["ipProto"] = "udp",
-    ["vlanid"] = 1,
+    ["vlanid"] = i,
     ["tcp_seq"] = i,
     ["pktSize"] = pkt_size,
     ["gtpu_teid"] = 0
@@ -32,7 +33,7 @@ for i=0,seq_cnt - 1 do
 end
 
 -- 128 is the largest burst value
--- https://github.com/pktgen/Pktgen-DPDK/blob/pktgen-21.11.0/app/pktgen-cmds.c#L2966
+-- https://github.com/pktgen/Pktgen-DPDK/blob/pktgen-22.04.1/app/pktgen-cmds.c#L2990
 pktgen.set("0", "burst", 128)
 
 pktgen.start("0")
